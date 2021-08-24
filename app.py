8c2ddd765e8fd8d1ec6c45662582a89076d8b433
unknown <rlopezr1302@alumno.ipn.mx>
2021-08-23 22:55:04 -0500

    first

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
+import posix
 from flask import Flask, render_template
+from flask_cors import CORS
+from dotenv import load_dotenv
+import psycopg2
+import os
+
+#PostgreSQL Database credentials loaded from the .env file
+DATABASE = os.getenv('DATABASE')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABSE_PASSWORD')
 
 app = Flask(__name__)
+
+#CORS implemented
+CORS(app) 
+
+try:
+    con = psycopg2.connect(
+        database=DATABASE,
+        user=DATABASE_USERNAME,
+        password = DATABASE_PASSWORD)
+
+    cur = con.cursor()
+
+except:
+    print('Error')
 
 @app.route('/')
 def index():
